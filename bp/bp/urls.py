1cdc85ca92f379dcc048a0a5149c9737b69a8ca0
허은희 <808_meaning@naver.com>
2021-07-01 18:00:33 +0900

    first commit

--- bp/bp/urls.py
+++ bp/bp/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 import blog.views
 
+# 뭔가 수정하려고 쓴 내용
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/' , include('blog.urls') ), 
